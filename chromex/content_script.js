76edb02e7816c6e07db0b341c609f929d653f87a
Nick Doty <npdoty@ischool.berkeley.edu>
2011-03-06 14:51:22 -0800

    content script inserts a button on the new message popup dialog

--- chromex/content_script.js
+++ chromex/content_script.js
@@ -1,4 +1,14 @@
 $(function() {
-    // Write jQuery code here to be executed when the page has loaded (and idled)
-
+	var encrypt_button = '<label class="uiButton uiButtonLarge"><input type="button" value="Encrypt" id="encrypt_button"></label>';
+	
+	console.log('test content script');
+		
+	$('a[href*="action=composerDialog"]').click(function(event) {
+		console.log('clicked New Message');
+		setTimeout( function() {
+			console.log('timer fired')
+			$('.pop_content .dialog_buttons').prepend(encrypt_button);
+		}, 1000);
+		return true;
+	});
 });
