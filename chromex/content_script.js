f0af33232e4a203f148e599ca81120fb19736370
Saad Mir <saadmir@gmail.com>
2012-04-14 21:50:24 -0400

    fbapp

--- chromex/content_script.js
+++ chromex/content_script.js
@@ -1,22 +1,39 @@
 $(function() {
-	var encrypt_button = '<label class="uiButton uiButtonLarge"><input type="button" value="Encrypt" id="encrypt_button"></label>';
-	
-	console.log('test content script');
-		
-	$('a[href*="action=composerDialog"]').click(function(event) {
-		console.log('clicked New Message');
-		setTimeout( function() {
-			console.log('timer fired')
-			$('.pop_content .dialog_buttons').prepend(encrypt_button);
-		}, 1000);
-		return true;
-	});
-	
-    // Write jQuery code here to be executed when the page has loaded (and idled) 	 
-    FB.init({
-	    appId  : '205641416119920',
-	    status : true, // check login status
-	    cookie : true, // enable cookies to allow the server to access the session
-	    xfbml  : true  // parse XFBML
-	});
+    jQuery('body').append('<div id="fb-root"></div>');
+    jQuery('body').append('<div id="extensionListener">this is a invisible div</div>');
+
+    var encrypt_button = '<label class="uiButton uiButtonLarge"><input type="button" value="Encrypt" id="encrypt_button"></label>';
+    
+    console.log('test content script');
+        
+    $('a[href*="action=composerDialog"]').click(function(event) {
+        console.log('clicked New Message');
+        setTimeout( function() {
+            console.log('timer fired')
+            $('.pop_content .dialog_buttons').prepend(encrypt_button);
+        }, 1000);
+        return true;
+    });
+
+    (function(d){
+        var js, 
+        id = 'facebook-jssdk', 
+        ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+    }(document));
+
+    (function(d){
+        var js, 
+        id = 'pgpkeys', 
+        ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = chrome.extension.getURL("js/fb_app.js");
+        ref.parentNode.appendChild(js);
+    }(document));
 });
+
+
